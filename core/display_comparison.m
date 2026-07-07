59e4bc32af274f4e9a56a81c00892a1da4a6bbd5
Palak Lohani <palak.lohani195@gmail.com>
2026-07-08 03:05:48 +0530

    added mask feature

--- core/display_comparison.m
+++ core/display_comparison.m
@@ -1,12 +1,16 @@
-function display_comparison(O,O_star,psnr_val,ssim_val, total_time)
+function display_comparison(img,O,O_star,psnr_val,ssim_val, total_time)
 
     figure('Color','w');
+
+    subplot(1,3,1)
+    imshow(uint8(img),[]);
+    title('Original');
     
-    subplot(1,2,1)
+    subplot(1,3,2)
     imshow(uint8(O),[]);
     title('Original');
     
-    subplot(1,2,2)
+    subplot(1,3,3)
     imshow(uint8(O_star),[]);
     title(sprintf('Reconstructed\nPSNR = %.2f dB, SSIM = %.4f\nTime = %0.2f s', psnr_val, ssim_val, total_time));
 
